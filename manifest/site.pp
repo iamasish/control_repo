b27f52d513bd8c902060187d396e536c28ee2021
iamasish <31787372+iamasish@users.noreply.github.com>
2020-03-15 17:49:25 -0700

    Update site.pp

--- manifest/site.pp
+++ manifest/site.pp
@@ -1,7 +1,7 @@
 node default {
   file {'/root/README':
-    ensure => file,
-    content => 'This is a readme'
-    owner => 'root'
-    }
+  ensure   => file,
+  conteste => 'This is readme',
+  owner    => 'root',
+  }
 }
